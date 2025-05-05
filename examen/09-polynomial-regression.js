550f0c5ec629e9291237ecd6b1be3d2c7962147d
dan <2995284560101@ingenieria.usac.edu.gt>
2025-05-05 09:03:28 -0600

    actualizar

--- examen/09-polynomial-regression.js
+++ examen/09-polynomial-regression.js
@@ -2,25 +2,23 @@ async function fit_predict_draw() {
     const { PolynomialRegression, joinArrays } = await import('https://luisespino.github.io/mlearnjs/mlearn.mjs');
 
     const myPolynomialRegression = await PolynomialRegression(); 
-    const model = new myPolynomialRegression(3);
+    const model = new myPolynomialRegression(3); // Exponente 3
 
-    const X = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const y = [1, 4, 1, 5, 3, 7, 2, 7, 4, 9];
+    // Datos del archivo pred.csv
+    const NO = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Columna 'NO'
+    const C = [400.5, 405.8, 410.9, 425.4, 450.1, 500.5, 575.7, 456.5, 320.1, 221.9]; // Columna 'C'
 
-    model.fit(X, y);
+    model.fit(NO, C);
 
-    yPredict = model.predict(X)
-    //console.log(yPredict);
-
-    const myjoinArrays = await joinArrays();
-    const arr = myjoinArrays('x', X, 'y', y, 'yPredict', yPredict);
+    const yPredict = model.predict(NO);
+    const mse = model.mse(C, yPredict); // Error cuadrático medio
 
     const log = document.getElementById('log');
-    const yPred = yPredict.map(num => parseFloat(num.toFixed(2)));
-    const mse = model.mse(y, yPredict);
-    const r2 = model.r2(y, yPredict);
-    log.innerHTML = 'X: '+X+'<br>y: '+y+'<br>yPredict: '+yPred;
-    log.innerHTML += '<br>MSE: '+mse+'<br>R2: '+r2;
+    log.innerHTML = 'MSE: ' + Math.floor(mse * 10) / 10; // Mostrar con 1 decimal sin aproximar
+
+    // Opcional: Gráfica (si se desea visualizar)
+    const myjoinArrays = await joinArrays();
+    const arr = myjoinArrays('x', NO, 'y', C, 'yPredict', yPredict);
 
     google.charts.load('current', {'packages':['corechart']});
     google.charts.setOnLoadCallback(drawChart);    
@@ -29,7 +27,8 @@ async function fit_predict_draw() {
         var options = {
             series: {
                 0: {type: 'scatter'},
-                1: {type: 'line', curveType: 'function'}}
+                1: {type: 'line', curveType: 'function'}
+            }
         };  
         var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
         chart.draw(data, options);         
